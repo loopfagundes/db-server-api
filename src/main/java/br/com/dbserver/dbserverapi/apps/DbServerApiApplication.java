fd0d93b8f4b02c73b5ba84514ad4da3a57a5e7a1
Luciano Fagundes <luciano.fagundes@dbserver.com.br>
2022-07-27 22:44:21 -0300

    iniciou o projeto de db-Server-API

---
+++ src/main/java/br/com/dbserver/dbserverapi/apps/DbServerApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.dbserver.dbserverapi.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServerApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/dbserver/dbserverapi/testsApps/DbServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.dbserverapi.testsApps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
