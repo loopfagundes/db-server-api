1fdab31606d0ede23ffe0353ca721c3b6cf36096
Luciano Fagundes <luciano.fagundes@dbserver.com.br>
2022-07-27 23:17:53 -0300

    alterei o caminho de endpoint para melhorar e adiciona mapping POST.

--- src/main/java/br/com/dbserver/dbserverapi/controllers/ServerController.java
+++ src/main/java/br/com/dbserver/dbserverapi/controllers/ServerController.java
@@ -4,6 +4,8 @@ import br.com.dbserver.dbserverapi.models.ServerModel;
 import br.com.dbserver.dbserverapi.repositories.ServerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -12,9 +14,15 @@ public class ServerController {
     @Autowired
     private ServerRepository serverRepository;
 
-    @GetMapping("/getServer")
+    @GetMapping("/meu_api")
     public List<ServerModel> getServer() {
         List<ServerModel> serverModels = serverRepository.findAll();
         return serverModels;
+    }
+
+    @PostMapping("/meu_api")
+    public ServerModel cadastrarServer(@RequestBody ServerModel serverModel) {
+        ServerModel serverModelSave = serverRepository.save(serverModel);
+        return serverModelSave;
     }
 }
